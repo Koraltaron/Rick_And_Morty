76bc8591c6f144a49be981bf4852d02cf80b858d
Clément Praud <clement.praud6@gmail.com>
2024-12-03 17:33:38 +0100

    Start creating the character filter

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import Cards from "../Cards"
 import "../../styles/home-page.css"
+import type { ChangeEvent } from "react";
 // import { useEffect, useState } from "react"
 import { useLoaderData } from "react-router-dom"
+import { useState } from "react";
 
 interface Character {
   image: string,
@@ -12,6 +14,19 @@ interface Character {
 function HomePage () {
   const data = useLoaderData() ;
   const characters = data.results as Character[];
+
+  const[text, setText] = useState<string>("");
+  console.log(text)
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    if(event.currentTarget) {
+      setText(event.currentTarget.value)
+    }
+  }
+
+  const filteredCharacters = characters.filter((character) => {
+    character.name.includes(text) && character.name.includes(text)
+  })
   
   /* Fetch with useEffect :
 
@@ -28,9 +43,9 @@ function HomePage () {
     <>
       <main>
         <h1>Rick and Morty</h1>
-        <input type="text" placeholder="Trouve ton personnage préféré"></input>
+        <input onChange={handleChange} type="text" placeholder="Trouve ton personnage préféré" value={text}></input>
         <section id="section-container">
-          {characters.map((el) => (
+          {filteredCharacters.map((el) => (
           <Cards name={el.name} image={el.image} key={el.id}/>)
           )}
         </section>
